100e2fef0db6f24724ba693fac137460a4d6d467
sonnemaf <fons.sonnemans@reflectionit.nl>
2024-12-13 14:08:13 +0100

    Employee moved to class libary

--- App105/MainPage.xaml.cs
+++ App105/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-using System.Collections.Generic;
+using ClassLibrary1;
+using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
 
 namespace App105;
@@ -9,17 +10,26 @@ namespace App105;
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
 public sealed partial class MainPage : Page {
-    public List<Employee> Employees { get; }
+    public ObservableCollection<Employee> Employees { get; } = new();
 
     public MainPage() {
+
         InitializeComponent();
 
+        Employees.Add(new Employee("Fons", 2000));
+        Employees.Add(new Employee("Jim", 5000));
+        Employees.Add(new Employee("Ellen", 4000));
 
-        Employees = new List<Employee> {
-                new Employee("Fons", 2000),
-                new Employee("Jim", 4000),
-                new Employee("Ellen", 3000),
-            };
+    }
 
+    private void ButtonAdd_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+        Employees.Add(new Employee($"Emp {Employees.Count}", 4000));
+
+    }
+
+    private void ButtonRaiseSalary_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+        if (listViewEmployees.SelectedItem is Employee emp) {
+            emp.Salary += 100;
+        }
     }
 }
---
+++ ClassLibrary1/Employee.cs
@@ -0,0 +1,25 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace ClassLibrary1;
+
+public partial class Employee : ObservableObject {
+
+    public Employee() {
+    }
+
+    public Employee(string name, decimal salary) {
+        this.Name = name;
+        this.Salary = salary;
+    }
+
+    [ObservableProperty]
+    public partial string Name { get; set; }
+
+    [ObservableProperty]
+    public partial decimal Salary { get; set; }
+
+    public override string ToString() => $"Employee Name = {Name}, Salary = {Salary}";
+}
